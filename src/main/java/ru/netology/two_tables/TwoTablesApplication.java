b5da8abdd305601da2d9109ef3f11551159130a2
Petrov Dmitriy <dmitr.petrov.spb@gmail.com>
2023-01-14 11:47:25 +0300

    Initial commit

---
+++ src/main/java/ru/netology/two_tables/TwoTablesApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.two_tables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoTablesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwoTablesApplication.class, args);
+    }
+
+}
